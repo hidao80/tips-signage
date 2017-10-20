31442a9e58badd3eb70e2822f5ca1ce6bb568d66
hidao <hidao80@users.noreply.github.com>
2017-10-21 07:55:53 +0900

    定数を settings.php に分離

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-define("CONTENTS_DIR","contents/");
+require("settings.php");
+
 $json = json_decode(file_get_contents("index.json"), true);
 $item = array_rand($json);
 
@@ -13,12 +14,11 @@ $markdown = file_get_contents(CONTENTS_DIR.$item['content']);
 <!doctype html>
 <html lang="ja">
 <head>
-	<meta charset="UTF-8" />
-	<title>TIPS SIGNAGE</title>
-	<script src="js/marked.min.js">document.getElementById("markdown").innerHTML = marked();</script>
+  <meta charset="UTF-8" />
+  <title>TIPS SIGNAGE</title>
+  <script src="js/marked.min.js">document.getElementById("markdown").innerHTML = marked(<?= $markdown; ?>);</script>
 </head>
 <body>
-	<div id="markdown"></div>
-	<?php echo $markdown; ?>
+  <div id="markdown"></div>
 </body>
 </html>
