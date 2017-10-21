f42271944e4a7dea1750f7abea9dfabf208c8881
hidao <hidao80@users.noreply.github.com>
2017-10-21 09:44:00 +0900

    ios_debug_js.js を追加

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 require("settings.php");
 
 $json = json_decode(file_get_contents("index.json"), true);
-var_dump($json);
-$item = array_rand($json);
-var_dump($item);
+//var_dump($json);
+$index = array_rand($json);
 
 if ($item['disapproval'] < rand(0, 100)) {
   // reload
 }
-
-$markdown = file_get_contents(CONTENTS_DIR.$item['content']);
+//print_r($json[$index]['content']);
+$markdown = file_get_contents(CONTENTS_DIR.$json[$index]['content']);
+//print_r($markdown);
 ?>
 
 <!doctype html>
@@ -18,9 +18,13 @@ $markdown = file_get_contents(CONTENTS_DIR.$item['content']);
 <head>
   <meta charset="UTF-8" />
   <title>TIPS SIGNAGE</title>
-  <script src="js/marked.min.js">document.getElementById("markdown").innerHTML = marked(<?= $markdown; ?>);</script>
+  <script src="js/is_debug_js.js"></script>
+  <script src="js/marked.min.js"></script>
 </head>
 <body>
-  <div id="markdown"></div>
+ <div id="markdown"></div>
+  <script>
+    document.getElementById("markdown").innerHTML = marked("<?= $markdown; ?>");
+  </script>
 </body>
 </html>
